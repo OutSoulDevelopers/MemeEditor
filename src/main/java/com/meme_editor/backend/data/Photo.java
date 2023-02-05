b796ab80c7768d67a2839ff405e739a946c71722
SlideHehe <slideow@gmail.com>
2023-02-05 03:28:11 +0300

    add lombok and docker-compose file for mongodb

--- src/main/java/com/meme_editor/backend/data/Photo.java
+++ src/main/java/com/meme_editor/backend/data/Photo.java
@@ -1,40 +1,14 @@
 package com.meme_editor.backend.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Objects;
+import java.time.LocalDate;
 
+@Data
 public class Photo {
-    @Id private String id;
-    private final Date date;
+    @Id
+    private String id;
+    private final LocalDate date;
     private final byte[] image;
-
-    public Photo(Date date,byte[] image){
-        this.date = date;
-        this.image = image;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, date, Arrays.hashCode(image));
-    }
-    public Date get_date(){
-        return this.date;
-    }
-    public byte[] get_image(){
-        return this.image;
-    }
-    @Override
-    public String toString() {
-        return "Image{" +
-                "id=" + this.id +
-                ", date='" + this.date + '\'' +
-                ", lastName='" + Arrays.toString(this.image) + '\'' +
-                '}';
-    }
-
 }
